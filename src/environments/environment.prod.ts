70c730a22c2c2016653e6146088601fcd8639bbd
Manikandan M <manikandan@aaludra.com>
2020-03-14 08:21:16 +0530

    second

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCrk6YcuPt3G5YyJ694_hozIpU1G77gnpQ",
+    authDomain: "pushnotification-c53dc.firebaseapp.com",
+    databaseURL: "https://pushnotification-c53dc.firebaseio.com",
+    projectId: "pushnotification-c53dc",
+    storageBucket: "pushnotification-c53dc.appspot.com",
+    messagingSenderId: "1021296588199",
+    appId: "1:1021296588199:web:4aec59544117631e33f282",
+    measurementId: "G-K2KZ8NDYP7"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCrk6YcuPt3G5YyJ694_hozIpU1G77gnpQ",
+    authDomain: "pushnotification-c53dc.firebaseapp.com",
+    databaseURL: "https://pushnotification-c53dc.firebaseio.com",
+    projectId: "pushnotification-c53dc",
+    storageBucket: "pushnotification-c53dc.appspot.com",
+    messagingSenderId: "1021296588199",
+    appId: "1:1021296588199:web:4aec59544117631e33f282",
+    measurementId: "G-K2KZ8NDYP7"
+  }
 };
 
 /*
